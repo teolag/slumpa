4072d6d55ab7165c917a926da3b248722a27fc2a
Teodor Lagerqvist <git@teodor.se>
2021-12-11 00:18:32 +0100

    Docs fixes

--- docs-src/main.ts
+++ docs-src/main.ts
@@ -2,20 +2,26 @@ import { Slumpa } from '../dist'
 const s = new Slumpa('boll')
 
 onFormSubmit('frmRandomInt', (form) => {
-  form.result.value = s.integer(parseInt(form.min.value), parseInt(form.max.value))
+  const min = parseInt(form.min.value)
+  const max = parseInt(form.max.value)
+  form.result.value = s.integer(min, max)
+  form.code.value = `const integer = slumpa.integer(${min}, ${max})`
 })
 
 onFormSubmit('frmRandomItem', (form) => {
   const animals = ['Horse', 'Monkey', 'Cow', 'Dog', 'Cat']
+  form.code.value = `const animal = slumpa.item(${JSON.stringify(animals)})`
   form.result.value = s.item(animals)
 })
 
 function onFormSubmit(formId: string, submitCallback: (form: HTMLFormElement) => void) {
   const form = getFormById(formId)
+  form.addEventListener('input', () => submitCallback(form))
   form.addEventListener('submit', (ev) => {
     ev.preventDefault()
     submitCallback(form)
   })
+  submitCallback(form)
 }
 
 function getFormById(id: string): HTMLFormElement {
